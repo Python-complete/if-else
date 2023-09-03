9e24f2b98916e3416ff5460a300eef60bdd9957b
Arpan Basu <arpanbasu2002@gmail.com>
2023-09-03 21:56:31 +0530

    Add files via upload

---
+++ if-else.py
@@ -0,0 +1,16 @@
+# if - else = taking decission
+n = int(input("Enter a number\n"))
+if n > 0 :
+    print("The entered number is positive")
+elif n < 0 :
+    print("The entered number is negative")
+else :
+    print("The entered number is zero")
+
+    '''
+        enter a number in n
+        check weather its greater than 0 
+        if yes print POSITIVE
+        if not again check weather its less than 0
+        if yes print NEGATIVE
+        if again the answer is no print ZERO'''
